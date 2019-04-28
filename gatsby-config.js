feb4913b08a7758fb01be54c7acdf8df17112d3f
Igor Soroka <stupbird@gmail.com>
2019-04-28 22:36:05 +0300

    changed info and profile picture

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `My Personal Blog`,
+    author: `Igor Soroka`,
+    description: `My personal blog about motivation/programming/sports`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `grenguar`,
     },
   },
   plugins: [
